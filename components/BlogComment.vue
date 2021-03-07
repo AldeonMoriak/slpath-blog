f22220547de31cd7fca7ee11a5057f9557e25e04
Aldeon Moriak <37976890+AldeonMoriak@users.noreply.github.com>
2021-03-07 11:47:00 +0330

    add comment prototype

---
+++ components/BlogComment.vue
@@ -0,0 +1,29 @@
+<!-- comment form -->
+<div class="flex mx-auto items-center justify-center shadow-lg mt-56 mb-4 max-w-lg border-t-8 border-purple-400 rounded-b-md" dir="rtl">
+   <form class="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+      <div class="flex flex-wrap -mx-3 mb-6">
+         <h2 class="px-4 pt-3 pb-2 text-purple-600 text-lg">ایجاد نظر</h2>
+         <div class="flex">
+         <div class="w-full md:w-full px-3 mb-2 mt-2">
+            <input class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600" name="body" placeholder='نام' required />
+         </div>
+          <div class="w-full md:w-full px-3 mb-2 mt-2">
+            <input type="email" dir="auto" class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600" name="body" placeholder='ایمیل' required />
+         </div>
+         </div>
+         <div class="w-full md:w-full px-3 mb-2 mt-2">
+            <textarea 
+            class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600" name="body" placeholder='چیزی بنویسید...' required></textarea>
+         </div>
+         
+         <div class="w-full flex items-start md:w-full px-3">
+            <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+               
+            </div>
+            <div class="-mr-1">
+               <input type='submit' class="bg-purple-400 text-purple-50 font-medium py-1 px-4 border border-purple-400 rounded-lg tracking-wide mr-1 hover:bg-purple-100 focus:border-purple-600" value='ثبت نظر'>
+            </div>
+         </div>
+      </form>
+   </div>
+</div>
