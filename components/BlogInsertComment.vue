1af2894750cf9553df49f80d62192563eb88e01e
lubuntuAldeon <mehranmirshekaran@protonmail.com>
2021-03-09 00:43:10 +0330

    add insert comment and comments section

--- components/BlogInsertComment.vue
+++ components/BlogInsertComment.vue
@@ -1,29 +1,95 @@
 <!-- comment form -->
-<div class="flex mx-auto items-center justify-center shadow-lg mt-56 mb-4 max-w-lg border-t-8 border-purple-400 rounded-b-md" dir="rtl">
-   <form class="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+<template>
+  <div
+    class="flex mx-auto items-center justify-center shadow-lg mb-4 border-t-8 border-purple-400 rounded-b-md font-wyekan"
+    dir="rtl"
+  >
+    <form class="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
       <div class="flex flex-wrap -mx-3 mb-6">
-         <h2 class="px-4 pt-3 pb-2 text-purple-600 text-lg">ایجاد نظر</h2>
-         <div class="flex">
-         <div class="w-full md:w-full px-3 mb-2 mt-2">
-            <input class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600" name="body" placeholder='نام' required />
-         </div>
+        <h2 class="px-4 pt-3 pb-2 text-purple-600 text-lg">ایجاد نظر</h2>
+        <div class="flex">
           <div class="w-full md:w-full px-3 mb-2 mt-2">
-            <input type="email" dir="auto" class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600" name="body" placeholder='ایمیل' required />
-         </div>
-         </div>
-         <div class="w-full md:w-full px-3 mb-2 mt-2">
-            <textarea 
-            class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600" name="body" placeholder='چیزی بنویسید...' required></textarea>
-         </div>
-         
-         <div class="w-full flex items-start md:w-full px-3">
-            <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
-               
-            </div>
-            <div class="-mr-1">
-               <input type='submit' class="bg-purple-400 text-purple-50 font-medium py-1 px-4 border border-purple-400 rounded-lg tracking-wide mr-1 hover:bg-purple-100 focus:border-purple-600" value='ثبت نظر'>
-            </div>
-         </div>
-      </form>
-   </div>
-</div>
+            <input
+              v-model="name"
+              class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600"
+              name="body"
+              placeholder="نام"
+              required
+            />
+          </div>
+          <div class="w-full md:w-full px-3 mb-2 mt-2">
+            <input
+              v-model="email"
+              type="email"
+              dir="rtl"
+              class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600"
+              name="body"
+              placeholder="ایمیل"
+              required
+            />
+          </div>
+        </div>
+        <div class="w-full md:w-full px-3 mb-2 mt-2">
+          <textarea
+            v-model="content"
+            class="bg-purple-50 rounded border border-purple-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white focus:border-purple-600"
+            name="body"
+            placeholder="چیزی بنویسید..."
+            required
+          ></textarea>
+        </div>
+
+        <div class="w-full flex items-start md:w-full px-3">
+          <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto"></div>
+          <div class="-mr-1">
+            <input
+              type="submit"
+              class="bg-purple-400 text-purple-50 font-medium py-1 px-4 border border-purple-400 rounded-lg tracking-wide mr-1 hover:bg-purple-100 focus:border-purple-600"
+              value="ثبت نظر"
+              @click.prevent="insertComment"
+            />
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    parentId: {
+      type: Number,
+      default: 0,
+    },
+    articleId: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      name: '',
+      content: '',
+      email: '',
+      message: '',
+    }
+  },
+  methods: {
+    async insertComment() {
+      if (!this.name || !this.content || !this.email) return
+      await this.$axios
+        .$post('/comments/insertComment', {
+          name: this.name,
+          email: this.email,
+          content: this.content,
+          parentId: this.parentId,
+          articleId: this.articleId,
+        })
+        .then((res) => {
+          this.message = res.message
+        })
+    },
+  },
+}
+</script>
--- pages/blog/_slug/_id.vue
+++ pages/blog/_slug/_id.vue
@@ -5,84 +5,96 @@
         class="w-full mx-auto mb-8 text-left md:w-3/4 lg:w-1/2"
       />
     </div>
-    <article
-      v-else
-      dir="rtl"
-      class="container px-4 py-14 mx-auto"
-      itemid="#"
-      itemscope
-      itemtype="http://schema.org/BlogPosting"
-    >
-      <div class="w-full mx-auto mb-8 md:w-3/4 lg:w-1/2 font-wyekan">
-        <img
-          :src="`${$axios.defaults.baseURL}/articles/image/${post.imageUrl}`"
-          class="object-cover w-full h-64 bg-center rounded-t-lg"
-          :alt="post.title"
-        />
-        <div class="bg-white rounded-b-xl py-0.5 pr-1">
-          <h1
-            class="mb-3 mt-1 text-xl font-bold leading-tight text-gray-900 md:text-2xl"
-            itemprop="headline"
-            :title="post.title"
-          >
-            {{ post.title }}
-          </h1>
-          <div class="flex mb-6 pr-2 space-x-2">
-            <a
-              v-for="tag in post.tags"
-              :key="tag.id"
-              class="text-purple-800 bg-purple-200 px-2 ml-1 rounded-full text-xs hover:bg-purple-300"
-              href="#"
-              >{{ tag.title }}</a
+    <div v-else class="w-full mx-auto md:w-3/4 lg:w-1/2 font-wyekan px-4">
+      <article
+        dir="rtl"
+        class="container py-14 mx-auto"
+        itemid="#"
+        itemscope
+        itemtype="http://schema.org/BlogPosting"
+      >
+        <div class="mb-8">
+          <img
+            :src="`${$axios.defaults.baseURL}/articles/image/${post.imageUrl}`"
+            class="object-cover w-full h-64 bg-center rounded-t-lg"
+            :alt="post.title"
+          />
+          <div class="bg-white rounded-b-xl py-0.5 pr-1 shadow-md">
+            <h1
+              class="mb-3 mt-1 text-xl font-bold leading-tight text-gray-900 md:text-2xl"
+              itemprop="headline"
+              :title="post.title"
             >
-          </div>
-          <div class="flex items-center text-gray-700">
-            <nuxt-link :to="`/blog/${post.admin.username}`">
-              <img
-                :src="`${$axios.defaults.baseURL}/image/${post.admin.profilePictureThumbnailUrl}`"
-                class="object-cover w-14 h-14 rounded-full shadow-sm text-sm mx-auto"
-                :alt="`عکس ${post.admin.name}`"
-              />
-            </nuxt-link>
-            <div class="flex justify-between w-full items-end">
-              <div class="mr-2">
-                <nuxt-link
-                  class="text-sm font-semibold text-gray-800"
-                  :to="`/blog/${post.admin.username}`"
+              {{ post.title }}
+            </h1>
+            <div class="flex mb-6 pr-2 space-x-2">
+              <a
+                v-for="tag in post.tags"
+                :key="tag.id"
+                class="text-purple-800 bg-purple-200 px-2 ml-1 rounded-full text-xs hover:bg-purple-300"
+                href="#"
+                >{{ tag.title }}</a
+              >
+            </div>
+            <div class="flex items-center text-gray-700">
+              <nuxt-link :to="`/blog/${post.admin.username}`">
+                <img
+                  :src="`${$axios.defaults.baseURL}/image/${post.admin.profilePictureThumbnailUrl}`"
+                  class="object-cover w-14 h-14 rounded-full shadow-sm text-sm mx-auto"
+                  :alt="`عکس ${post.admin.name}`"
+                />
+              </nuxt-link>
+              <div class="flex justify-between w-full items-end">
+                <div class="mr-2">
+                  <nuxt-link
+                    class="text-sm font-semibold text-gray-800"
+                    :to="`/blog/${post.admin.username}`"
+                  >
+                    {{ post.admin.name }}
+                  </nuxt-link>
+                  <p class="text-sm text-gray-500 mt-1">
+                    {{ post.createdDateTime | moment('jYYYY/jMM/jDD') }}
+                  </p>
+                </div>
+                <p
+                  class="ml-2 mb-1 text-xs font-semibold tracking-wider text-gray-500"
                 >
-                  {{ post.admin.name }}
-                </nuxt-link>
-                <p class="text-sm text-gray-500 mt-1">
-                  {{ post.createdDateTime | moment('jYYYY/jMM/jDD') }}
+                  {{ post.category.title }}
                 </p>
               </div>
-              <p
-                class="ml-2 mb-1 text-xs font-semibold tracking-wider text-gray-500"
-              >
-                {{ post.category.title }}
-              </p>
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="w-full mx-auto max-w-prose md:w-3/4 lg:w-1/2">
-        <div
-          class="font-wyekan bg-white shadow-sm p-5 rounded-xl ck-content"
-          dir="rtl"
-          v-html="post.content"
-        ></div>
-      </div>
-    </article>
+        <div class="">
+          <div
+            class="font-wyekan bg-white shadow-md p-5 rounded-xl ck-content"
+            dir="rtl"
+            v-html="post.content"
+          ></div>
+        </div>
+      </article>
+      <blog-comment
+        v-for="message in post.comment"
+        :key="message.id"
+        :message="message"
+        class="mb-6"
+      />
+      <blog-insert-comment :article-id="post.id" :parent-id="id" />
+    </div>
   </div>
 </template>
 
 <script>
+import BlogComment from '~/components/BlogComment.vue'
+import BlogInsertComment from '~/components/BlogInsertComment.vue'
 export default {
   name: 'BlogPost',
+  components: { BlogInsertComment, BlogComment },
   data() {
     return {
       post: null,
+      id: null,
     }
   },
   async fetch() {
